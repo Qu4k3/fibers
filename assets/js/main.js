36926384ed4359fb536349b9425457b594a60015
Qu4k3 <paul.guillamon@gmail.com>
2017-07-02 21:31:48 +0200

    update

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+
+    $(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
+
+    $('.modal').modal({
+	    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+	    opacity: .5, // Opacity of modal background
+	    in_duration: 300, // Transition in duration
+	    out_duration: 200, // Transition out duration
+	    starting_top: '4%', // Starting top style attribute
+	    ending_top: '10%', // Ending top style attribute
+	    }
+	  );
+
+});
