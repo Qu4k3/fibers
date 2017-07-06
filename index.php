95fe423d1fdac8e73d7f23836a46f3a2f793a118
Qu4k3 <paul.guillamon@gmail.com>
2017-07-06 12:18:11 +0200

    update

--- index.php
+++ index.php
@@ -41,118 +41,12 @@
   </script>
 </head>
 <body>
-  <section class="nav-box">
 
-    <div class="header">Fibers.cat</div>
-
-    <div class="sub-header">
-      <button class="btn btn-contribute waves-effect waves-light modal-trigger" data-target="send-info">Aporta contingut<i class="material-icons right">send</i></button>
-    </div>
-
-    <div class="content">
-      <table class="assignatures">
-        <tbody>
-          <tr class="clickable-row" data-href="url://">
-              <td>Física</td>
-              <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Fundaments Matematics</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Introducció als Computadors</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Programació 1</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Estructura de Computadors</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Matemàtiques 1</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Matemàtiques 2</td>
-            <td><i class="fa fa-circle feble tooltipped" data-position="right" data-delay="50" data-tooltip="Poc contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Programació 2</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Base de dades</td>
-            <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Interfaces de Computadors</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Estructura de Dades i Algorismes</td>
-            <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Probabilitat i Estadistica</td>
-            <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Sistemes Operatius</td>
-            <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Arquitectura de Computadors</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Empresa i Entorn Economic</td>
-            <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Introducció a la Ingenieria del Software</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Projectes de Programació</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Xarxes de Computadors</td>
-            <td><i class="fa fa-circle feble tooltipped" data-position="right" data-delay="50" data-tooltip="Poc contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Interacció i Diseny d"Interficies</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-          <tr class="clickable-row" data-href="url://">
-            <td>Paralelisme</td>
-            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
-    <footer>
-      <div class="footer">
-        <a class="social facebook" href="https://www.facebook.com/fibers.cat/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-        <a class="social mail" href="mailto:fiberscat@gmail.com" target="_blank"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
-        <a class="legal" href="#legal"><i class="fa fa-gavel" aria-hidden="true"></i></a>
-      </div>
-
-      <div class="sub-footer">
-        2016 - 2017 © Fibers.cat
-      </div>
-    </footer>
-
-  </section>
+  <?php include "views/layouts/_leftNavBar.php" ?>
 
   <section class="page">
     <div class="header">
-      <a class="waves-effect waves-light btn btn-assignatures" href="#assignatures"><i class="material-icons left">view_list</i>Llistat d'assignatures</a>
+      <a class="waves-effect waves-light btn btn-assignatures" href="assignatures"><i class="material-icons left">view_list</i>Llistat d'assignatures</a>
       <a class="waves-effect waves-light btn btn-usuaris" href="#usuaris"><i class="material-icons left">perm_identity</i>Usuaris col·laboradors</a>
     </div>
   </section>
@@ -440,15 +334,15 @@
       <section class="modal-content">
           <h4><span class="modal_title">Condicions d'ús</span></h4>
           <br>
-          <p>Fibers.cat es una plataforma que facilita els usuaris,
-            aportar contingut i fer-ho accesible per tothom.</p>
+          <p>Fibers.cat es una plataforma que facilita als usuaris
+            aportar contingut i fer-ho accesible per a tothom.</p>
 
           <p>A l'hora d'adjuntar fitxers, tingueu en compte:</p>
 
           <ul>
             <li><span>No s'accepta material facilitat pel professor</li>
-            <li><span>Material amb copyright (llibres, etc.)</span></li>
-            <li><span>Material sense el consentiment del propietari</span></li>
+            <li><span>No s'accepta material amb copyright (llibres, etc.)</span></li>
+            <li><span>No s'accepta material sense el consentiment del propietari</span></li>
           </ul>
       </section>
       <section class="modal-footer">
---
+++ views/layouts/_leftNavBar.php
@@ -0,0 +1,110 @@
+<section class="nav-box">
+
+  <div class="header">
+    <a href="/">Fibers.cat</a>
+  </div>
+
+  <div class="sub-header">
+    <button class="btn btn-contribute waves-effect waves-light modal-trigger" data-target="send-info">Aporta contingut<i class="material-icons right">send</i></button>
+  </div>
+
+  <div class="content">
+    <table class="assignatures">
+      <tbody>
+        <tr class="clickable-row" data-href="url://">
+            <td>Física</td>
+            <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Fundaments Matematics</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Introducció als Computadors</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Programació 1</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Estructura de Computadors</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Matemàtiques 1</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Matemàtiques 2</td>
+          <td><i class="fa fa-circle feble tooltipped" data-position="right" data-delay="50" data-tooltip="Poc contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Programació 2</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Base de dades</td>
+          <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Interfaces de Computadors</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Estructura de Dades i Algorismes</td>
+          <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Probabilitat i Estadistica</td>
+          <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Sistemes Operatius</td>
+          <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Arquitectura de Computadors</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Empresa i Entorn Economic</td>
+          <td><i class="fa fa-circle tooltipped" data-position="right" data-delay="50" data-tooltip="Hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Introducció a la Ingenieria del Software</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Projectes de Programació</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Xarxes de Computadors</td>
+          <td><i class="fa fa-circle feble tooltipped" data-position="right" data-delay="50" data-tooltip="Poc contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Interacció i Diseny d"Interficies</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="clickable-row" data-href="url://">
+          <td>Paralelisme</td>
+          <td><i class="fa fa-circle-o tooltipped" data-position="right" data-delay="50" data-tooltip="No hi ha contingut" aria-hidden="true"></i></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <footer>
+    <div class="footer">
+      <a class="social facebook" href="https://www.facebook.com/fibers.cat/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+      <a class="social mail" href="mailto:fiberscat@gmail.com" target="_blank"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+      <a class="legal" href="#legal"><i class="fa fa-gavel" aria-hidden="true"></i></a>
+    </div>
+
+    <div class="sub-footer">
+      2016 - 2017 © Fibers.cat
+    </div>
+  </footer>
+
+</section>
