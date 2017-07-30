7350f39f2591d96edc7d60f667cc0419e8b86733
Qu4k3 <paul.guillamon@gmail.com>
2017-07-30 14:33:51 +0200

    debugged

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,32 @@
 jQuery(document).ready(function($) {
 
-    $(".clickable-row").click(function() {
-        window.location = $(this).data("href");
-    });
+  $(".clickable-row").click(function() {
+      window.location = $(this).data("href");
+  });
 
-    $('.modal').modal({
-	    dismissible: true, // Modal can be dismissed by clicking outside of the modal
-	    opacity: .5, // Opacity of modal background
-	    in_duration: 300, // Transition in duration
-	    out_duration: 200, // Transition out duration
-	    starting_top: '4%', // Starting top style attribute
-	    ending_top: '10%', // Ending top style attribute
-	    }
-	  );
+  $('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    in_duration: 300, // Transition in duration
+    out_duration: 200, // Transition out duration
+    starting_top: '4%', // Starting top style attribute
+    ending_top: '10%', // Ending top style attribute
+    }
+  );
 
-    $('input#name').characterCounter();
+  $('input#name').characterCounter();
 
-    $("#navbar-toggle").click( function (){
-      if( $('.nav-box').css('left') === '-325px' )  {
-          $(".nav-box").css("left", '0px');
-          $(".nav-box").css("width", '88%');
-      }
-      else {
-        $(".nav-box").css("left", '-325px');
-        $(".nav-box").css("width", '325px');
-      }
+  $("#navbar-toggle").click( function (){
+    if( $('.nav-box').css('left') === '-325px' )  {
+        $(".nav-box").css("left", '0px');
+        $(".nav-box").css("width", '88%');
+    }
+    else {
+      $(".nav-box").css("left", '-325px');
+      $(".nav-box").css("width", '325px');
+    }
 
-    });
+  });
 
 });
 
